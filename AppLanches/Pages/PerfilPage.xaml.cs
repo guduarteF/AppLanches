493b1f1c1ce573d6226e73caa0311188aea0952d
Guduarte <gustavo64duarte@gmail.com>
2024-08-27 20:32:10 -0300

    Implementação visual da pagina de perfil

--- AppLanches/Pages/PerfilPage.xaml.cs
+++ AppLanches/Pages/PerfilPage.xaml.cs
@@ -6,4 +6,29 @@ public partial class PerfilPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ImgBtnPerfil_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void TapPedidos_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void MinhaConta_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Perguntas_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void BtnLogout_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
